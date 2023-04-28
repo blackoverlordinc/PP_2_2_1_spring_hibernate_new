9926500dcf71195c58ad895dd7b9c627a368d9cc
Sergey Kadushkin <blackoverlord.inc@mail.ru>
2023-04-28 13:21:32 +0300

    init com

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -2,9 +2,14 @@ package hiber.service;
 
 import hiber.model.User;
 
+
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
+
+    User getUserByCarNumber(String model, int series);
 }
